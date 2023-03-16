47023a8c7712ec23a53cfde72b38f5fb675f1b43
Noonever <noonevernowhere@gmail.com>
2023-03-16 13:20:33 +0300

    Request system  created

--- services.py
+++ services.py
@@ -1,4 +1,4 @@
-from random import choices
+from random import choice, choices
 from string import ascii_uppercase
 import orjson
 from loguru import logger
@@ -9,14 +9,8 @@ def video_is_being_processed(code: str) -> bool:
     return REDIS_CLIENT.hexists(REQUEST_QUEUE_HASH, code)
 
 
-def video_is_recently_created(code: str) -> bool:
-    return REDIS_CLIENT.sismember(RECENT_VIDEOS_SET, code)
-
-
 def video_is_stored(code: str) -> bool:
-    with open(PROCESSED_VIDEO_DATA, 'rb') as f:
-        stored_videos = orjson.loads(f.read())
-    return code in stored_videos
+    return REDIS_CLIENT.sismember(VIDEO_CODES_SET, code)
 
 
 def get_video_status(code: str) -> int:
@@ -25,7 +19,7 @@ def get_video_status(code: str) -> int:
     """
     if video_is_being_processed(code):
         return 2
-    elif video_is_recently_created(code) or video_is_stored(code):
+    elif video_is_stored(code):
         return 0
     else:
         return 1
@@ -46,14 +40,31 @@ def genetate_unique_code() -> str:
     return code
 
 
-def create_generation_request(text: str) -> str:
+def get_random_video_path() -> Path:
+    video_paths = list(SOURCE_VIDEO_DIR.rglob("*.mp4"))
+    random_path = choice(video_paths)
+    return random_path.absolute()
+
+
+def get_random_phrase() -> str:
+    with open(PHRASES_FILE, "rb") as f:
+        phrase = choice(orjson.loads(f.read()))
+    return phrase
+
+
+def create_generation_request(name: str) -> str:
     """
-    Returns code of a video
+    Creates video generation request and returns its code
     """
     code = genetate_unique_code()
+    text = f"{name.capitalize()}, {get_random_phrase()}"
+    video_path = get_random_video_path()
+
     REDIS_CLIENT.rpush(REQUEST_QUEUE_LIST, code)
-    REDIS_CLIENT.hset(REQUEST_QUEUE_HASH, code, text)
+    REDIS_CLIENT.hset(REQUEST_QUEUE_HASH, code, f"{text}[,!,]{video_path}")
+    # "[,!,] is used for splitting string in video processing program"
     return code
+
 
 # Testing
 code_list = []
@@ -63,9 +74,10 @@ while True:
         for code in code_list:
             logger.info(f"Code: {code}, status: {get_video_status(code)}")
     elif command =="red":
-        logger.debug(f"Generated: {REDIS_CLIENT.smembers(RECENT_VIDEOS_SET)}")
+        logger.debug(f"Generated: {REDIS_CLIENT.smembers(VIDEO_CODES_SET)}")
         logger.debug(f"Queue: {REDIS_CLIENT.llen(REQUEST_QUEUE_LIST)}")
         logger.debug(f"Queue hash: {REDIS_CLIENT.hgetall(REQUEST_QUEUE_HASH)}")
     else:
         new_code = create_generation_request(command)
         code_list.append(new_code)
+
--- settings.py
+++ settings.py
@@ -4,14 +4,12 @@ import redis
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 SOURCE_VIDEO_DIR = BASE_DIR / 'media/source_videos'
-SOURCE_VIDEO_DATA = SOURCE_VIDEO_DIR/'data.json'
-
 PROCESSED_VIDEO_DIR = BASE_DIR / 'media/processed_videos'
-PROCESSED_VIDEO_DATA = PROCESSED_VIDEO_DIR/'data.json'
+PHRASES_FILE = BASE_DIR / 'media/phrases.json'
 
 REDIS_CLIENT = redis.Redis(host='localhost', port=6379, db=0)
 
 # Names of redis storages
 REQUEST_QUEUE_LIST = "generation_queue"
 REQUEST_QUEUE_HASH = "generation_queue_hash"
-RECENT_VIDEOS_SET = "recently_created_videos_set"
+VIDEO_CODES_SET = "stored_video_codes"
